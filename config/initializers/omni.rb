6c7a2be8dee19697dce295f44f5763827aa72dde
fdwillis <fdwillis7@icloud.com>
2024-07-04 00:26:42 -0500

    redirect_uri

--- config/initializers/omni.rb
+++ config/initializers/omni.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['client_id'], ENV['client_secret']
   {
-    redirect_uri: 'http://localhost:3000/users/auth/google_oauth2/callback'
+    redirect_uri: Rails.env.development? ? 'http://localhost:3000/users/auth/google_oauth2/callback' : 'https://vast-cliffs-75108-216bc25e33c6.herokuapp.com/auth/google_oauth2/callback'
   }
 end
 OmniAuth.config.allowed_request_methods = %i[get]
