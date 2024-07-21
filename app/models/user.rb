c85a60eae7ca6538bb15259685c7c6b33fe21cc8
fdwillis <fdwillis7@icloud.com>
2024-07-21 14:37:24 -0500

    content and traffic

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,7 +62,7 @@ class User < ApplicationRecord
       user = User.find_or_create_by(email: data['email'],name: data['name'])
       user.update(password: Devise.friendly_token[0,20],auth_token: access_token.credentials.token, fresh_token: access_token.extra.id_token)
       
-      unless user.uuid.present?
+      unless user&.uuid.present?
          user.update(uuid: SecureRandom.uuid)
       end
 
